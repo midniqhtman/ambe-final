fcb2436402a839c46dca73df788309d8673879ea
midniqhtman <c1esi@mail.ru>
2023-01-01 23:03:21 +0300

    Initial Commit

---
+++ AmbeSwiftUI/AmbeSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AmbeSwiftUIApp.swift
+//  AmbeSwiftUI
+//
+//  Created by Байсаев Зубайр on 01.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct AmbeSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
